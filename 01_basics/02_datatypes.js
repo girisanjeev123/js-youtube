84953fa01e91a8497808691d53b2ee85a986e307
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-16 10:50:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"//treat all java code as a new version
+// alert(3+3)// we are using nodejs not a browser
+console.log(3
+    +3)// code redeability should be high
+    let name = "sanjeev"
+    let age= 24
+    let isloggedin = false
+    //number => 2 power 53//
+   // biginit//
+    //string => ""
+    //boolean => true/false
+    // null => standalone value
+    //undefined =>
+        //symbol => unique
+    console.log("type of undefined");//undefined
+    console.log("type of null ");// object
+   
