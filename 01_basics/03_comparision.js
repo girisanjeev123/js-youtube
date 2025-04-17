fa046eb8d231dd2f53fda0da6c581bbf21c690f8
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-17 09:26:28 +0000

    comparision

---
+++ 01_basics/03_comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>3)
+// console.log(2!=4)
+// console.log(2==2)
+// console.log(4>2)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// equal to and comparision operator works differently.
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined>=0)
+
+
