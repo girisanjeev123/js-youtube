083b03de2d2d6f2366146a7811b570f4277961a1
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-18 16:43:46 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+
+//array
+const array = [1,2,3,4]
+const array1 = ["ironman","captain america","hulk"]
+console.log(array[1])
+console.log(array1[1])
+//array methods
+// array.push(6)
+// array.push(7)
+// array.pop()
+// array.unshift(8)
+//  array.shift()
+// console.log(array.includes(6))
+// console.log(array.indexOf(3))
+// const newarray = array.join()
+// console.log(array)
+// console.log(typeof newarray)
+
+//slice and splice
+
+console.log('A',array)
+const newarray2 = array.slice(1,2)
+console.log('B',newarray2)
+const newarray3 = array.splice(1,2)
+console.log('C',newarray3)
+console.log(array)
