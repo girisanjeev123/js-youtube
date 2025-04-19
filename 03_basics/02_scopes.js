867a28fd8f3cb2fd83f30d1822a10bd4d99eaa73
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-19 12:40:57 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+// let a = 20;
+// const b = 30;
+// var c = 40;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+ 
+// let a = 300;
+// if(true) {
+//    let a = 20;
+//    const b = 30;
+//    console.log(b);
+// }
+// console.log(a);
+
+// if(true){
+//    const username = "sanjeev"
+//    if(username === "sanjeev"){
+//       const website = "youtube"
+//       console.log(username+website);
+//       console.log(website);
+//    }
+//    console.log(username);
+// }
