fe487e4e8f9e81e35b4edcc0520a18ac1c4f5431
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-19 10:21:02 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function sayMyName(){
+console.log("S");
+console.log("A");
+console.log("N");
+console.log("J");
+console.log("E");
+console.log("E");
+console.log("V");
+}
+//sayMyName()
+//addition of two numbers in function
+// function addTwoNumbers(numb1, numb2){
+//     console.log(numb1+numb2);
+// }
+// addTwoNumbers(6, 7)
+
+function calculateChartPrice(...numb1){
+    return numb1;
+}
+//console.log(calculateChartPrice(500, 200, 600, 800, 100));
+
+// const myNewArray = [100, 200, 300, 400, 500]
+// function returnSecondValue(getArray){
+// return(getArray[1]);
+// }
+// console.log(returnSecondValue(myNewArray));
