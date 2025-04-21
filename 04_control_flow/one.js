b08e07b7d463c0bd0187d2095fdf595020d99e6a
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-21 05:54:14 +0000

    if,elseif

---
+++ 04_control_flow/one.js
@@ -0,0 +1,46 @@
+//if
+//> ,< ,>= ,<= ,== ,!== ,!= ,=== 
+
+// syntax
+// if(true){
+
+// }
+// const temperature = 50;
+// if(temperature<60){
+//     console.log("then the temperature is too high.");
+// }
+// console.log("the temperature is low");
+
+// const score = 200;
+//  if(score>100){
+//     let power = "fly";
+//     console.log(`userpower:${fly}`);
+//  }
+//  console.log(`userpower:${fly}`);
+
+// const balance = 1000;
+
+// if(balance <500){
+//     console.log("the balance is less than 500.");
+// }
+// else if(balance < 700){
+//     console.log("the balance is less than 700.");
+// }
+// else if(balance <900){
+//     console.log("the balance is less than 900.")
+// }
+// else
+//    { console.log("the balance is less than 1200.")}
+
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+// if(userLoggedIn && debitCard && 2==3  ){
+//     console.log("allow to buy course.");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("user loggedin.");
+// }
+
+
