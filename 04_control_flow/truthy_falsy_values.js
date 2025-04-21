f67bdec015ef6f61b1bdc364d9bc3369ae775652
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-21 09:04:05 +0000

    Truthy and falsy values

---
+++ 04_control_flow/truthy_falsy_values.js
@@ -0,0 +1,33 @@
+//const userEmail = []
+
+// if(userEmail)
+// {
+//     console.log("userEmail exit.");
+// }
+
+//falsy values
+//0, "", null, NaN, false, -0, undefined, BigInt 0n,
+//truthy values
+//"0", [],{}, " ", "false", function(){}
+// if(userEmail.length===0){
+//     console.log("array is empty.")
+// }
+
+// const emptyObj = {}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("object is empty");
+// }
+// null coalecising operator(??):null undefined
+
+//let val1;
+//val1 = 5??10;
+//val1 = null??10;
+//val1 = undefined??15;
+//val1 = null??10??20;
+//console.log(val1);
+
+//terniary operator
+//syntax
+//condition?true:false
+//const iceBagPrice = 100;
+//iceBagPrice >= 80?console.log("price is less"):console.log("price is more.")
