bd13d06b11419dc3391ed1c78d92d6b2373e4082
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-04-22 08:30:50 +0000

    for of for in & for each loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,40 @@
+//for of loop
+
+// const array = [1 ,2 ,3 ,4 ,5 ];
+// for (const num of array) {
+//     console.log( num)
+// }
+// const code = "hello world"
+ //for (const result of code) {
+  //  console.log(`${ result}`)
+// }
+ //Map
+//  const map = new Map()
+//  map.set('in' ,"india")
+//  map.set(' usa',"united states of america")
+//  map.set('fr' ,"france")
+ //console.log(map)
+//  for (const [key , value]of map)  {
+//     console.log(key ,":-" ,value)
+//  }
+//code for for in loop
+// const myObject = {
+//    js : "javascript",
+//    cpp : "c++",
+//    rb : "ruby",
+//    swift : "swift by apple"
+// }
+// for (const key in myObject) {
+//    console.log(`${key}shortcut is for ${myObject[key]}`);
+   
+// }
+    
+// NOTE:
+// for...of → used with arrays, strings, maps, etc.
+
+// for...in → used with objects to loop through keys
+//for each loop
+const coding =["js" ,"ruby" ,"python" ,"java" ,"react"]
+coding.forEach(function (value){
+   console.log(value)
+})
