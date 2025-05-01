92c120f5b4a21c681a4ebbf8b8633b01c743ef33
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-05-01 10:41:29 +0000

    filters and map

---
+++ 05_iterations/Three.js
@@ -0,0 +1,15 @@
+// const myNums = [1,2,3,4,5,6,7,8,9]
+// const newNums =  myNums.filter((num) => num > 4)
+// console.log(newNums)
+
+// const myNums = [1,2,3,4,5,6,7,8,9]
+// const newNums = myNums.map((num) => num + 10);
+// console.log(newNums);
+
+//chaninng
+// const myNums = [1,2,3,4,5,6,7,8,9]
+// const newNums = myNums
+//                      .map((num) => num*10 )
+//                      .map((num) => num+1)
+//                     .filter((num) => num>20)
+//                      console.log(newNums)
