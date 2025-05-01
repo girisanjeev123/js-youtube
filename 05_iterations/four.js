f97d8b515850c591cec3b73e4196539aaa21054b
girisanjeev123 <138343652+girisanjeev123@users.noreply.github.com>
2025-05-01 11:47:03 +0000

    reduce

---
+++ 05_iterations/four.js
@@ -0,0 +1,8 @@
+//reduce method
+const myNum = [1,2,3]
+//const myTotal = myNum.reduce(function(acc,currval){
+  //  console.log(`acc: ${acc} and currval: ${currval}`);
+ //   return acc + currval;
+//},0)
+// const myTotal = myNum.reduce((acc,curr) => acc+curr,0)
+// console.log(myTotal)
